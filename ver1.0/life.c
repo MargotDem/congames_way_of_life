047f49fe6a751f1ed981828f96121571377c962f
Margot De maulmont <mde-maul@c1r6p12.hive.fi>
2022-06-23 14:00:48 +0300

    bitwise version

--- ver1.0/life.c
+++ ver1.0/life.c
@@ -71,14 +71,97 @@ static int	get_x_dimension(char *str)
 	return (i);
 }
 
+int	find_neighbors(t_uint16 **grid, int y, int i, uint16_t bitwise_nb, int grid_size_x, int grid_size_y)
+{
+	//find_neighbors(init_grid, y, i, bitwise_nb);
+	int	neighbors;
+	int j;
+	int k;
+	u_int16_t	nb_minus;
+	u_int16_t	nb_plus;
+
+	/*
+
+	y - 1  i - 1
+	y  i - 1
+	y + 1  i - 1
+
+	y - 1  i
+	y + 1  i
+
+	y - 1  i + 1
+	y i + 1
+	y + 1  i + 1
+
+	*/
+
+	neighbors = 0;
+
+	if (i > 0)
+	{
+		j = i - 1;
+		nb_minus = bitwise_nb << 1;
+		if (nb_minus == 0b0000000000000000)
+			nb_minus = 0b0000000000000001;
+
+		if (y > 0)
+		{
+			if (grid[y - 1][j / 16] & nb_minus)
+				neighbors++;
+		}
+		if (grid[y][j / 16] & nb_minus)
+			neighbors++;
+		if (y < grid_size_y - 1)
+		{
+			if (grid[y + 1][j / 16] & nb_minus)
+				neighbors++;
+		}
+	}
+
+	if (y > 0)
+	{
+		if (grid[y - 1][i / 16] & bitwise_nb)
+			neighbors++;
+	}
+	if (y < grid_size_y - 1)
+	{
+		if (grid[y + 1][i / 16] & bitwise_nb)
+			neighbors++;
+	}
+
+	if (i < grid_size_x - 1)
+	{
+		k = i + 1;
+		nb_plus = bitwise_nb >> 1;
+		if (nb_plus == 0b0000000000000000)
+			nb_plus = 0b1000000000000000;
+		
+		if (y > 0)
+		{
+			if (grid[y - 1][k / 16] & nb_plus)
+				neighbors++;
+		}
+		if (grid[y][k / 16] & nb_plus)
+			neighbors++;
+		if (y < grid_size_y - 1)
+		{
+			if (grid[y + 1][k / 16] & nb_plus)
+				neighbors++;
+		}	
+	}
+	return (neighbors);
+}
+
 void	play(int grid_size_x, int grid_size_y, t_uint16 **grid,  t_uint16 **grid2, int uint16_nb, int iterations)
 {
 	int	i;
 	int	y;
 	int	x;
 	int	turns;
+	int	neighbors;
 	t_uint16	**init_grid;
 	t_uint16	**rslt_grid;
+	t_uint16	**tmp;
 	uint16_t	bitwise_nb;
 	(void)uint16_nb;
 
@@ -100,26 +183,42 @@ void	play(int grid_size_x, int grid_size_y, t_uint16 **grid,  t_uint16 **grid2, 
 				{
 					rslt_grid[y][i / 16] ^= bitwise_nb;
 				}
-				if ((init_grid[y][i / 16] & bitwise_nb) && turns == 0)
+				if ((init_grid[y][i / 16] & bitwise_nb))
 				{
-					rslt_grid[y][i / 16] = rslt_grid[y][i / 16] | bitwise_nb;
+					neighbors = find_neighbors(init_grid, y, i, bitwise_nb, grid_size_x, grid_size_y);
+					if (neighbors == 2 || neighbors == 3)
+					{
+						//printf("nb of live neighbors is %d\n", neighbors);
+						rslt_grid[y][i / 16] = rslt_grid[y][i / 16] | bitwise_nb;
+					}
+				}
+				else
+				{
+					neighbors = find_neighbors(init_grid, y, i, bitwise_nb, grid_size_x, grid_size_y);
+					if (neighbors == 3)
+					{
+						rslt_grid[y][i / 16] = rslt_grid[y][i / 16] | bitwise_nb;
+					}
 				}
 				bitwise_nb = bitwise_nb >> 1;
 				if (bitwise_nb == 0b0000000000000000)
 					bitwise_nb = 0b1000000000000000;
 				i++;
 			}
-			printf("\n");
 			y++;
 		}
-		printf("\n");
-		printf("turn is %d, init grid:\n", turns);
-		print_grid(grid_size_x, grid_size_y, init_grid);
-		printf("\nrslt grid:\n");
-		print_grid(grid_size_x, grid_size_y, rslt_grid);
+		//printf("\n");
+		//printf("grid size y is %d\n", y);
+		//printf("turn is %d, init grid:\n", turns);
+		//print_grid(grid_size_x, grid_size_y, init_grid);
+		//printf("\nrslt grid:\n");
+		//print_grid(grid_size_x, grid_size_y, rslt_grid);
 		turns++;
+		tmp = rslt_grid;
+		rslt_grid = init_grid;
+		init_grid = tmp;
 	}
-	init_grid = rslt_grid;
+	print_grid(grid_size_x, grid_size_y, rslt_grid);
 }
 
 void	print_grid(int grid_size_x, int grid_size_y, t_uint16 **grid)
@@ -197,10 +296,9 @@ void	make_grid(char *initial_state, int iterations)
 		set_bits2(&initial_state[y * (grid_size_x + 1)], grid_size_x, grid, y);
 		y++;
 	}
-	print_grid(grid_size_x, grid_size_y, grid);
-	printf("\n\n");
-	print_grid(grid_size_x, grid_size_y, grid2);
-	printf("\n\nplay:");
+	//print_grid(grid_size_x, grid_size_y, grid);
+	//printf("\n\n");
+	//print_grid(grid_size_x, grid_size_y, grid2);
 	play(grid_size_x, grid_size_y, grid, grid2, uint16_nb, iterations);
 	printf("\n\n");
 }
